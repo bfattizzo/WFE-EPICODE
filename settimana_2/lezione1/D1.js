22fc11f813533e0e2d0528bb9ad7c79b17ce4e2d
bfattizzo <barbara.fattizzo@gmail.com>
2023-11-21 15:53:21 +0100

    aggiornamento

--- settimana_2/lezione1/D1.js
+++ settimana_2/lezione1/D1.js
@@ -12,6 +12,16 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+/* - string è una sequenza di caratteri senza specifiche */
+let string; 
+/*- number è di tipo numerico, può essere un numero intero o decimale*/
+let number; 
+/*- Boolean è un tipo di valore vero o falso */
+let Boolean; 
+/*- null è indicato per i valori nulli o vuoti */
+let age = null; 
+/*- undefined viene utilizzato per quei valori indefiniti */
+let varible = undefined;
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
@@ -19,19 +29,26 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 let myName = 'Barbara';
-console.log(myName); 
+console.log('myName'); 
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let numero1 = 12;
+let numero2 = 20; 
+let somma = numero1 + numero2;
+console.log(somma) = somma;
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let name = x; 
+let numero = 12;
+document.write(numero);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
@@ -39,12 +56,17 @@ console.log(myName);
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const myName = 'Fattizzo'; 
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let a = 4; 
+let x = 12; 
+let sottrazione = a - x; 
+console.log(sottrazione) = sottrazione; 
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -53,3 +75,7 @@ console.log(myName);
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = 'john';
+let name2 = 'John';
+let verifica = name1 === name2;
+console.log ('i due nomi sono uguali?' + verifica);
---
+++ settimana_2/lezione2/esercizio/js/D2.js
@@ -0,0 +1,112 @@
+/* ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+var myArray = [1, 3, 6 , 9];
+var m = Math.max(myArray);
+console.log(m)
+
+/* ESERCIZIO 2
+  Scrivi un algoritmo che mostri "not equal" in console se un numero intero fornito è diverso da 5.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 3
+  Scrivi un algoritmo che mostri "divisibile per 5" in console se un numero fornito è perfettamente divisibile per 5 (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let numero1 = 25;
+let numero2 = 5;
+var risultato = 25%5;
+
+/* ESERCIZIO 4
+  Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let e = 2;
+let f = 6; 
+let sottrazione= e - f;
+if (e === 8){
+  console.log('e è pari a 8')
+}
+if (f === 8){
+  console.log('f è pari a 8')
+}
+if (somma === 8){
+  console.log('la somma di '+ e +' più '+ f +' è pari a 8')
+}
+if (sottrazione === 8){
+  console.log('la sottrazione di '+ e +' e '+ f +' è pari a 8')
+}
+
+/* ESERCIZIO 5
+  Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
+  C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
+  Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 6
+  Stai lavorando su un sito di e-commerce. Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
+  Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando come prima se le spedizioni sono gratuite oppure no e e calcolando il totale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 7
+  Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
+  Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.
+  Alla fine mostra il risultato in console.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 8
+  Crea un algoritmo per verificare che un valore fornito sia un numero oppure no (suggerimento: cerca su un motore di ricerca "typeof").
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 9
+  Crea un algoritmo per controllare se un numero fornito sia pari o dispari (suggerimento: cerca l'operatore modulo su un motore di ricerca)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 10
+  Modifica la logica del seguente algoritmo in modo che mostri in console il messaggio corretto in ogni circostanza.
+  let val = 7
+  if (val < 10) {
+      console.log("Meno di 10");
+    } else if (val < 5) {
+      console.log("Meno di 5");
+    } else {
+      console.log("Uguale a 10 o maggiore");
+    }
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 11
+  Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
+*/
+
+const me = {
+  name: 'John',
+  lastName: 'Doe',
+  skills: ['javascript', 'html', 'css'],
+}
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 12
+  Lavorando sempre sull'oggetto precedentemente fornito, scrivi del codice per rimuovere la proprietà "lastName".
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
